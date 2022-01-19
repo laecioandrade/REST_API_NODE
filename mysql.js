c75644e915d8be4c5a80c0c77cd997f259602e3d
Laécio Rodrigues <37408950+laecioandrade@users.noreply.github.com>
2022-01-18 22:38:56 -0300

    Refactoring code, added best practices.

--- mysql.js
+++ mysql.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql');
 
 var pool = mysql.createPool({
+    "connectionLimit": 100,
     "user": process.env.MYSQL_USER,
     "password": process.env.MYSQL_PASSWORD,
     "database": process.env.MYSQL_DATABASE,
@@ -8,5 +9,16 @@ var pool = mysql.createPool({
     "port": process.env.MYSQL_PORT
 });
 
+exports.execute = (query, params=[]) => {
+    return new Promise((resolve, reject) => {
+        pool.query(query, params, (error, result, fields) => {
+            if(error){
+                reject(error);
+            }else{
+                resolve(result);
+            }
+        })
+    })
+}
 
 exports.pool = pool;
