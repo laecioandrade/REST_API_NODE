450c937c461ddb5407314d5dd4c5565fb8066c9c
Laécio Rodrigues <37408950+laecioandrade@users.noreply.github.com>
2022-01-24 13:41:32 -0300

    Creating Foreign Key and creating Categories

--- database/db_version.sql
+++ database/db_version.sql
@@ -1,7 +1,46 @@
--- 20/01/2022
-CREATE TABLE IF NOT EXISTS imagens_produtos (
-    id_imagem INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    id_produto INT,
-    caminho VARCHAR(255),
-    FOREIGN KEY (id_produto) REFERENCES produtos (id_produto)
+
+--create tables
+CREATE TABLE IF NOT EXISTS products (
+    productId INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(45),
+    price FLOAT,
+    productImage VARCHAR(255)
 );
+
+CREATE TABLE IF NOT EXISTS orders (
+    orderId INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    productId INT,
+    quantity INT,
+    FOREIGN KEY (productId) REFERENCES products (productId)
+);
+
+CREATE TABLE IF NOT EXISTS users (
+    userId INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(500) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS productImages (
+    imageId INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    productId INT,
+    path VARCHAR(255),
+    FOREIGN KEY (productId) REFERENCES products (productId)
+);
+
+CREATE TABLE IF NOT EXISTS categories (
+    categoryId INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100)
+);
+
+ALTER TABLE products ADD categoryId INT NULL;
+
+INSERT INTO categories (name) VALUES ('Material Escolar');
+
+UPDATE products SET categoryId = 1;
+
+ALTER TABLE products MODIFY categoryId INT NOT NULL;
+
+ALTER TABLE products ADD CONSTRAINT fk_product_category FOREIGN KEY (categoryId) REFERENCES categories(categoryId);
+
+SELECT * FROM products;
+
